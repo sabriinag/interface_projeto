d4673d2787e3b90d8cbca6428ae95bfa1bcfa766
Sabrina <goncalvessabrina74@gmail.com>
2023-10-02 20:32:42 -0300

    ganhe muito dinheiro faca sucesso e melhore o mundo

---
+++ src/main/java/br/com/digitalsolutions/biblioteca/Biblioteca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.digitalsolutions.biblioteca;
+
+/**
+ *
+ * @author 823215404
+ */
+public class Biblioteca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
